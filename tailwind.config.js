c1aa4d3c3c3c87f553d6bd2b3de82144a86626bf
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-01-13 16:05:33 +0530

    Setup tailwindcss to the project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+    "./modules/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
